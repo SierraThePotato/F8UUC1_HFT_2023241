343a771e914f67f401d9a81687ebdfb480185112
Szilágyi Csaba <szilagyics@stud.uni-obuda.hu>
2023-11-19 13:33:03 +0100

    Create IRepository

---
+++ F8UUC1_HFT_2023241.Repository/Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F8UUC1_HFT_2023241.Repository
+{
+    public abstract class Repository
+    {
+    }
+}
