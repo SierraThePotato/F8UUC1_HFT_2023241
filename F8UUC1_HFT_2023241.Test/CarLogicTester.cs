9f16e0adc3b294295f95d8b760f6fc1b4d141401
Szilágyi Csaba <szilagyics@stud.uni-obuda.hu>
2023-11-30 21:40:18 +0100

    Fix test

---
+++ F8UUC1_HFT_2023241.Test/CarLogicTester.cs
@@ -0,0 +1,15 @@
+﻿using F8UUC1_HFT_2023241.Logic;
+using NUnit.Framework;
+using System;
+
+namespace F8UUC1_HFT_2023241.Test
+{
+    public class CarLogicTester
+    {
+        CarLogic logic;
+
+        [SetUp]
+
+
+    }
+}
--- F8UUC1_HFT_2023241.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace F8UUC1_HFT_2023241.Test
-{
-    public class Class1
-    {
-    }
-}
