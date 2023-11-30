650e66e3a3f1a7e5dd22024bace0ff16aedc831d
Szilágyi Csaba <szilagyics@stud.uni-obuda.hu>
2023-11-30 21:30:49 +0100

    Create Test

---
+++ CarDb.Test/CarLogicTester.cs
@@ -0,0 +1,17 @@
+﻿using F8UUC1_HFT_2023241.Logic;
+using NUnit.Framework;
+using System;
+
+namespace CarDb.Test
+{
+    public class CarLogicTester
+    {
+        CarLogic logic;
+
+        [SetUp]
+        public void Init()
+        {
+
+        }
+    }
+}
